d9ada82e65575d8a1f5d3ad79f62fd973071faa5
ajones119 <56804418+ajones119@users.noreply.github.com>
2020-11-07 18:38:52 -0600

    adding some 4x4 AI

--- Tic-Tac-Toe-Game/Tic-Tac-Toe-Game/ClientApp/src/app/game-board/game-board.component.ts
+++ Tic-Tac-Toe-Game/Tic-Tac-Toe-Game/ClientApp/src/app/game-board/game-board.component.ts
@@ -133,7 +133,14 @@ export class GameBoardComponent implements OnInit {
  
   makeRandomAIMove(AIplayer: string) {
     console.log("MakeRandomAIMove start");
-    var index: number = Math.floor(Math.random() * (8 - 0 + 1)) + 0;
+
+    if (this.BoardSize == 3) {
+      var index: number = Math.floor(Math.random() * (8 - 0 + 1)) + 0;
+    }
+    else if (this.BoardSize == 4) {
+      var index: number = Math.floor(Math.random() * ( - 0 + 1)) + 0;
+
+    }
     console.log("AI Check Square " + index);
     if (!this.board[index]) {
       console.log("finish Check = space available");
@@ -227,6 +234,19 @@ export class GameBoardComponent implements OnInit {
   }
 
   checkWin(board: string[]) {
+    if (this.BoardSize == 3) {
+      return this.checkWin3x3(board);
+    }
+    else if (this.BoardSize == 4) {
+      return this.checkWin4x4(board);
+    }
+  }
+
+
+  checkWin3x3(board: string[]) {
+
+    //Rows
+
     if (board[0] === board[1] && board[0] === board[2] && board[0]) {
       return { winner: board[0], cells: [0, 1, 2] };
     }
@@ -237,6 +257,8 @@ export class GameBoardComponent implements OnInit {
       return { winner: board[6], cells: [6, 7, 8] };
     }
 
+    //Columns
+
     if (board[0] === board[3] && board[0] === board[6] && board[0]) {
       return { winner: board[0], cells: [0, 3, 6] };
     }
@@ -246,6 +268,8 @@ export class GameBoardComponent implements OnInit {
     if (board[2] === board[5] && board[2] === board[8] && board[2]) {
       return { winner: board[2], cells: [2, 5, 8] };
     }
+
+    //Diagonals
 
     if (board[0] === board[4] && board[0] === board[8] && board[0]) {
       return { winner: board[0], cells: [0, 4, 8] };
@@ -259,6 +283,118 @@ export class GameBoardComponent implements OnInit {
     }
     return false;
   }
+
+  checkWin4x4(board: string[]) {
+    //Rows
+
+    if (board[0] === board[1] && board[0] === board[2]  && board[0] === board[3] && board[0]) {
+      return { winner: board[0], cells: [0, 1, 2, 3] };
+    }
+
+    if (board[4] === board[5] && board[4] === board[6] && board[4] === board[7] && board[4]) {
+      return { winner: board[4], cells: [4, 5, 6, 7] };
+    }
+
+    if (board[8] === board[9] && board[8] === board[10] && board[8] === board[11] && board[8]) {
+      return { winner: board[8], cells: [8, 9, 10, 11] };
+    }
+
+    if (board[12] === board[13] && board[12] === board[14] && board[12] === board[15] && board[12]) {
+      return { winner: board[12], cells: [12, 13, 14, 15] };
+    }
+
+    //Columns
+
+    if (board[0] === board[4] && board[0] === board[8] && board[0] === board[12] && board[0]) {
+      return { winner: board[0], cells: [0, 4, 8, 12] };
+    }
+
+    if (board[1] === board[5] && board[1] === board[9] && board[1] === board[13] && board[1]) {
+      return { winner: board[1], cells: [1, 5, 8, 13] };
+    }
+
+    if (board[2] === board[6] && board[2] === board[10] && board[2] === board[14] && board[2]) {
+      return { winner: board[2], cells: [2, 6, 10, 14] };
+    }
+
+    if (board[3] === board[7] && board[3] === board[11] && board[3] === board[15] && board[3]) {
+      return { winner: board[3], cells: [3, 7, 11, 15] };
+    }
+
+    //Diagonals
+
+    if (board[0] === board[5] && board[0] === board[10] && board[0] === board[15] && board[0]) {
+      return { winner: board[0], cells: [0, 5, 10, 15] };
+    }
+
+    if (board[3] === board[6] && board[3] === board[9] && board[3] === board[12] && board[3]) {
+      return { winner: board[3], cells: [3, 6, 9, 12] };
+    }
+
+    //Squares
+
+    if (board[0] === board[1] && board[0] === board[4] && board[0] === board[5] && board[0]) {
+      return { winner: board[0], cells: [0, 1, 4, 5] };
+    }
+
+    if (board[2] === board[3] && board[2] === board[6] && board[2] === board[7] && board[2]) {
+      return { winner: board[2], cells: [2, 3, 6, 7] };
+    }
+
+    if (board[8] === board[9] && board[8] === board[12] && board[8] === board[13] && board[8]) {
+      return { winner: board[8], cells: [8, 9, 12, 13] };
+    }
+
+    if (board[10] === board[11] && board[10] === board[14] && board[10] === board[15] && board[10]) {
+      return { winner: board[10], cells: [10, 11, 14, 15] };
+    }
+
+    if (board[1] === board[2] && board[1] === board[5] && board[1] === board[6] && board[1]) {
+      return { winner: board[1], cells: [1, 2, 5, 6] };
+    }
+
+    if (board[4] === board[5] && board[4] === board[8] && board[4] === board[9] && board[4]) {
+      return { winner: board[4], cells: [4, 5, 8, 9] };
+    }
+
+    if (board[9] === board[10] && board[9] === board[13] && board[9] === board[14] && board[9]) {
+      return { winner: board[9], cells: [9, 10, 13, 14] };
+    }
+
+    if (board[6] === board[7] && board[6] === board[10] && board[6] === board[11] && board[6]) {
+      return { winner: board[6], cells: [6, 7, 10, 11] };
+    }
+
+    if (board[5] === board[6] && board[5] === board[9] && board[5] === board[10] && board[5]) {
+      return { winner: board[5], cells: [5, 6, 9, 10] };
+    }
+
+    //Diamonds
+
+    if (board[1] === board[4] && board[1] === board[6] && board[1] === board[9] && board[1]) {
+      return { winner: board[1], cells: [1, 4, 6, 9] };
+    }
+
+    if (board[2] === board[5] && board[2] === board[7] && board[2] === board[10] && board[2]) {
+      return { winner: board[2], cells: [2, 5, 7, 10] };
+    }
+
+    if (board[5] === board[8] && board[5] === board[10] && board[5] === board[13] && board[5]) {
+      return { winner: board[5], cells: [5, 8, 10, 13] };
+    }
+
+    if (board[6] === board[9] && board[6] === board[11] && board[6] === board[14] && board[6]) {
+      return { winner: board[6], cells: [6, 9, 11, 14] };
+    }
+
+
+    if (board.every(cell => cell !== null)) {
+      return { winner: 'draw' };
+    }
+    return false;
+  }
+
+
 
   writeWinner(winner) {
     this.isGameOver = true;
